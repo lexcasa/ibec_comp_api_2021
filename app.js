9ceac4036bf946e9bb893b25f2e38073ce71fe13
Alex Casadevall <alex@lexartlabs.com>
2021-06-04 20:53:20 -0300

    udpate repo

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
+// Importo el modulo de express para nuestra app.js
+// Lo hago disponible globalmente dentro de la app
 const express = require('express')
+
+// invocar el express dentro la app
 const app = express()
+
+// Defino el puerto donde va a escuchar esta API REST
 const port = 3000
 
+// Defino el estado
+// Metodo, Ruta, Input/Output
+/*
+	- Metodo: .get 
+	- Ruta: /
+	- Input: req
+	- Outpu: res 
+
+	Estado : Raiz (o root)
+
+*/
 app.get('/', (req, res) => {
+
+  // Solo devuelve una respuesta
+  // Con texto plano
   res.send('Hello World!')
 })
 
